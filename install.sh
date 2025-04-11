2062119fcd22833669c5322553632f634d9bc2ba
Kansikas <145750653+lehucloud@users.noreply.github.com>
2025-04-11 10:01:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 docker compose run -it --rm \
     -e ENABLE_SQLITE=false \
     -e ENABLE_REDIS=false \
-    -e ADMIN_ACCOUNT=admin@demo.com \
+    -e ADMIN_ACCOUNT=admin@qmm.best \
     web php artisan xboard:install
